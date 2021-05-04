77b90ae00dce1844de058c8cee8a8338a0889765
Wyatt Simmons <simmonsw182@gmail.com>
2021-05-03 21:30:38 -0700

    commented out all db queries

--- server.js
+++ server.js
@@ -40,16 +40,16 @@ const db = mysql.createConnection(
 // });
 
 // Create a candidate
-const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-              VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//               VALUES (?,?,?,?)`;
+// const params = [1, 'Ronald', 'Firbank', 1];
 
-db.query(sql, params, (err, result) => {
-  if (err) {
-    console.log(err);
-  }
-  console.log(result);
-});
+// db.query(sql, params, (err, result) => {
+//   if (err) {
+//     console.log(err);
+//   }
+//   console.log(result);
+// });
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
